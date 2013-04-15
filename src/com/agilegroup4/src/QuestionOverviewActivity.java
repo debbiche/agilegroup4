8791ad767b49bef17c2bbb58e70b992aee23c50e
schroeja <schroeja@gmail.com>
2013-04-15 16:54:07 +0200

    added login activity

--- src/com/agilegroup4/src/QuestionOverviewActivity.java
+++ src/com/agilegroup4/src/QuestionOverviewActivity.java
@@ -2,31 +2,21 @@ package com.agilegroup4.src;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 
-public class LoginActivity extends Activity {
+public class QuestionOverviewActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_question_overview);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
+		getMenuInflater().inflate(R.menu.question_overview, menu);
 		return true;
 	}
 
-	public void loginButton(){
-		String userID;
-
-		
-		
-		Intent intent = new Intent(this, QuestionOverviewActivity.class);
-		startActivity(intent);
-	}
-	
 }

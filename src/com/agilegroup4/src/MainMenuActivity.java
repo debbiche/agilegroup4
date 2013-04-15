102f3ecc657126bd586c51421a2bbc98a30bcd7d
JGrunden <it4grjo@student.chalmers.se>
2013-04-15 16:41:37 +0200

    Add buttons

--- src/com/agilegroup4/src/MainMenuActivity.java
+++ src/com/agilegroup4/src/MainMenuActivity.java
@@ -1,8 +1,9 @@
 package com.agilegroup4.src;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainMenuActivity extends Activity {
 
@@ -17,6 +18,26 @@ public class MainMenuActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
+	}
+	
+	public void handleMenuItemOnClick(View view){
+		switch(view.getId()){
+		case R.id.button_profile: 
+			// Go to profile activity
+			break;
+		case R.id.button_questions: 
+			// Go to questions activity
+			break;
+		case R.id.button_users: 
+			// Go to user activity
+			break;
+		case R.id.button_tags: 
+			// Go to tags activity
+			break;
+		case R.id.button_settings: 
+			// Go to settings activity
+			break;
+		}
 	}
 
 }

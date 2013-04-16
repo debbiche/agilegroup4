32b8f37e68fdea5ec7c3e8d28d5dcbe58b106bec
JGrunden <it4grjo@student.chalmers.se>
2013-04-16 16:04:28 +0200

    Fix mainmenu bug

--- src/com/agilegroup4/src/MainMenuActivity.java
+++ src/com/agilegroup4/src/MainMenuActivity.java
@@ -16,8 +16,6 @@ public class MainMenuActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
 	

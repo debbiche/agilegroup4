08ad755f7ddd2b664ec85d2dc594413db03eabfe
Anmar Khazal <anmarkhazal@gmail.com>
2013-04-18 16:34:49 +0200

    Add LoginTest

--- agilegroup4Test-test/src/com/agilegroup4/src/test/LoginTest.java
+++ agilegroup4Test-test/src/com/agilegroup4/src/test/LoginTest.java
@@ -1,32 +1,53 @@
 package com.agilegroup4.src.test;
 
-import android.app.Activity;
+import junit.framework.Assert;
 import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.agilegroup4.src.*;
+import com.agilegroup4.src.LoginActivity;
+import com.agilegroup4.src.QuestionOverviewActivity;
 
 public class LoginTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
+	// Start monitor on intended activity.
+	ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LoginActivity.class.getName(), null, false);
+
+	// Open login activity
+	LoginActivity loginActivity = getActivity();
+	
 	public LoginTest() {
 		super("com.agilegroup4.src", LoginActivity.class);
 	}
-
+	
 	protected void setUp() throws Exception {
 		super.setUp(); 
+	}
+	
+	/**
+	 * Tests if userLogin field input is saved correctly
+	 */	
+	public void testLoginInput() throws Throwable {
+		
+		//Get the input field from the userlogin activity
+		final EditText userInputText = (EditText) loginActivity.findViewById(com.agilegroup4.src.R.id.text_login_username);
+		
+		//Create test input
+		String input = "1";	
+		
+		//Insert input into input field
+		userInputText.setText(input);		
+		
+		//Assert that input field contains input
+		Assert.assertTrue(userInputText.getText().toString().equals(input));	
+		
 	}
 	
 	/**
 	 * Test ???.
 	 */
 	public void testLoginActivity() {
-		// Start monitor on intended activity.
-		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LoginActivity.class.getName(), null, false);
-
-		// Open login activity
-		LoginActivity loginActivity = getActivity();
 		
 		// Get the navigation option for user profile activity
 		final Button button = (Button) loginActivity.findViewById(com.agilegroup4.src.R.id.button_login);

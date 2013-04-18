85a1e39cad6f344002a2fbeecf505577a783d123
schroeja <schroeja@gmail.com>
2013-04-18 13:35:21 +0200

    little commenting

--- src/com/agilegroup4/src/LoginActivity.java
+++ src/com/agilegroup4/src/LoginActivity.java
@@ -43,18 +43,20 @@ public class LoginActivity extends Activity {
 
 	public void loginButton(View view){
 		// read username and save in String userID
+		// TODO: catch NullPointerException here
 		EditText editText = (EditText) findViewById(R.id.text_login_username);
 		String inputTest = editText.getText().toString();
 		int userID = Integer.parseInt(inputTest);
-		// TODO: catch NullPointerException here
 		
-		// TODO: check for username in DB
+		// check for username in DB
 		if(dbHandler.userExists(userID)){
+			
 			// login successful --> open Question Overview Activity and hand over "user data"
 			Intent intent = new Intent(this, QuestionOverviewActivity.class);
 			intent.putExtra(EXTRA_USERNAME, userID);	// TODO: this might be a different field and should be obtained from DB
 			startActivity(intent);
 		} else {
+			
 			AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
 			alert.setTitle("Sorry!");
@@ -62,15 +64,9 @@ public class LoginActivity extends Activity {
 			
 			alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int whichButton) {
-			}});
-			
+			}});	
 			
 			alert.show();
-		}
-		
-		
-		
-		
+		}	
 	}
-	
 }

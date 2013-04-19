2d293d0fd0490099b2d53da02dee2781eae094d0
Mikael Johansson <mikajoha85@gmail.com>
2013-04-19 13:41:44 +0200

    Did some cleaning of 3 classes.

--- src/com/agilegroup4/src/QuestionOverviewActivity.java
+++ src/com/agilegroup4/src/QuestionOverviewActivity.java
@@ -2,7 +2,6 @@ package com.agilegroup4.src;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;

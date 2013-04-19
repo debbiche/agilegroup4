40d88686a566477b628a1f1aa2730bc20ce96f2f
JGrunden <it4grjo@student.chalmers.se>
2013-04-19 11:32:29 +0200

    Add first test of listview

--- src/com/agilegroup4/src/QuestionOverviewActivity.java
+++ src/com/agilegroup4/src/QuestionOverviewActivity.java
@@ -1,32 +1,41 @@
 package com.agilegroup4.src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class QuestionOverviewActivity extends Activity{
+public class QuestionOverviewActivity extends Activity {
 
-    // This is the Adapter being used to display the list's data
-    SimpleCursorAdapter mAdapter;
-	
+	// This is the Adapter being used to display the list's data
+	SimpleCursorAdapter mAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_question_overview);
-		
-		SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
+
+		SharedPreferences settings = getSharedPreferences(
+				LoginActivity.PREFS_NAME, 0);
 		int userID = settings.getInt("userID", 0);
-		
-	    ((TextView)findViewById(R.id.pop_text)).setText("Hej " + LoginActivity.dbHandler.getUserById(userID).getDisplay_name() + " how's it hanging?"); 
-	
-	    displayQuestions();
+
+		// ((TextView)findViewById(R.id.pop_text)).setText("Hej " +
+		// LoginActivity.dbHandler.getUserById(userID).getDisplay_name() +
+		// " how's it hanging?");
+
+		displayQuestions();
 	}
 
 	@Override
@@ -35,30 +44,77 @@ public class QuestionOverviewActivity extends Activity{
 		getMenuInflater().inflate(R.menu.question_overview, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Intent intent;
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.menuitem_mainmenu:
-				intent = new Intent(this, MainMenuActivity.class);
-				startActivity(intent);
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menuitem_mainmenu:
+			intent = new Intent(this, MainMenuActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
-	public void displayQuestions(){
+
+	public void displayQuestions() {
 		LoginActivity.dbHandler.queryQuestions();
 		ArrayList<Question> questions = LoginActivity.dbHandler.getQuestions();
-		TextView rowOne = (TextView) findViewById(R.id.oneText);
-		TextView rowTwo = (TextView) findViewById(R.id.twoText);
-		rowOne.setText(questions.get(1).getTitle());
-		rowTwo.setText(questions.get(2).getTitle());
+		final ListView listview = (ListView) findViewById(R.id.listview);
+		final ArrayList<String> titles = new ArrayList<String>();
+		for (int i = 0; i < 10; i++) {
+			titles.add(questions.get(i).getTitle());
+		}
+
+		final StableArrayAdapter adapter = new StableArrayAdapter(this,
+				android.R.layout.simple_list_item_1, titles);
+		listview.setAdapter(adapter);
+
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+				final String item = (String) parent.getItemAtPosition(position);
+				/*
+				view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
+							@Override
+							public void run() {
+								titles.remove(item);
+								adapter.notifyDataSetChanged();
+								view.setAlpha(1);
+							}
+						});*/
+			}
+
+		});
 	}
 
+	private class StableArrayAdapter extends ArrayAdapter<String> {
 
+		HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+		public StableArrayAdapter(Context context, int textViewResourceId,
+				List<String> objects) {
+			super(context, textViewResourceId, objects);
+			for (int i = 0; i < objects.size(); ++i) {
+				mIdMap.put(objects.get(i), i);
+			}
+		}
+
+		@Override
+		public long getItemId(int position) {
+			String item = getItem(position);
+			return mIdMap.get(item);
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+	}
 
 }

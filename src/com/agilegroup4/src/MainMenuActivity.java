9cd6e6f2ccfba67bcf8e81d122ff6668cd850e92
JGrunden <it4grjo@student.chalmers.se>
2013-04-19 15:26:51 +0200

    Add logout button 2.0

--- src/com/agilegroup4/src/MainMenuActivity.java
+++ src/com/agilegroup4/src/MainMenuActivity.java
@@ -1,6 +1,8 @@
 package com.agilegroup4.src;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -20,39 +22,41 @@ public class MainMenuActivity extends Activity {
 		// The menu option is not available in this activity
 		return true;
 	}
-	
-	public void handleMenuItemOnClick(View view){
+
+	public void handleMenuItemOnClick(View view) {
 		Intent intent;
-		switch(view.getId()){
-		case R.id.button_profile: 
+		switch (view.getId()) {
+		case R.id.button_profile:
 			intent = new Intent(this, UserProfileActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.button_questions: 
+		case R.id.button_questions:
 			intent = new Intent(this, QuestionOverviewActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.button_users: 
+		case R.id.button_users:
 			intent = new Intent(this, UsersActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.button_tags: 
+		case R.id.button_tags:
 			intent = new Intent(this, TagsOverviewActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.button_settings: 
+		case R.id.button_settings:
 			intent = new Intent(this, SettingsActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.button_logout: 
-			// Add logout functionality
-			SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
-		    SharedPreferences.Editor editor = settings.edit();
-		    editor.putInt("userID", 0);
-		    editor.commit();
+		case R.id.button_logout:
+
+			SharedPreferences settings = getSharedPreferences(
+					LoginActivity.PREFS_NAME, 0);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putInt("userID", 0);
+			editor.commit();
 			intent = new Intent(this, LoginActivity.class);
 			intent.putExtra("action", "Logout");
 			startActivity(intent);
+
 			break;
 		}
 	}

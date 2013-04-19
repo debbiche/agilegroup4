284385616c65dfd4916d2fca8cd7a0eca3ed7ea2
Anmar Khazal <anmarkhazal@gmail.com>
2013-04-19 10:34:03 +0200

    Add separate class LoginInputTest

---
+++ agilegroup4Test-test/src/com/agilegroup4/src/test/LoginInputTest.java
@@ -0,0 +1,49 @@
+package com.agilegroup4.src.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.agilegroup4.src.LoginActivity;
+
+
+
+public class LoginInputTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+	public LoginInputTest() {
+		super("com.agilegroup4.src", LoginActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp(); 
+	}
+
+	/**
+	 * Tests if userLogin field input is saved correctly
+	 */	
+	public void testLoginInput() throws Throwable {
+
+		// Open login activity
+		LoginActivity loginActivity = getActivity();
+
+		//Get the input field from the userlogin activity
+		final EditText userInputText = (EditText) loginActivity.findViewById(com.agilegroup4.src.R.id.text_login_username);
+
+		loginActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run(){
+				//Create test input
+				String input = "1";	
+
+				//Insert input into input field
+				userInputText.setText(input);		
+
+				//Assert that input field contains input
+				Assert.assertTrue(userInputText.getText().toString().equals(input));	
+			}
+		});
+	}
+	}
+
+	

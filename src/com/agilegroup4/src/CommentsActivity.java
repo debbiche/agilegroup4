d42b5cddfac0d087d9dc98892f5bb623ac105f01
Markus Pettersson <pmarkus@student.chalmers.se>
2013-04-23 15:30:37 +0200

    Added some classes and methods for viewing answers and comments, They are not finished however.

---
+++ src/com/agilegroup4/src/CommentsActivity.java
@@ -0,0 +1,42 @@
+package com.agilegroup4.src;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class CommentsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_comments);
+		
+		int action = getIntent().getIntExtra("questionId", 0);
+		TextView view = (TextView) findViewById(R.id.question_id);
+		view.setText("Question id: " + action);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.comments, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent intent;
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.menuitem_mainmenu:
+				intent = new Intent(this, MainMenuActivity.class);
+				startActivity(intent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+}

6c9ec959b48496a3bc7802024fc11bec7dfad13f
JGrunden <it4grjo@student.chalmers.se>
2013-04-26 13:25:28 +0200

    Update question test

--- agilegroup4Test-test/src/com/agilegroup4/src/test/QuestionIdTest.java
+++ agilegroup4Test-test/src/com/agilegroup4/src/test/QuestionIdTest.java
@@ -11,6 +11,7 @@ import android.widget.ListView;
 import com.agilegroup4.model.Question;
 import com.agilegroup4.src.DatabaseHandler;
 import com.agilegroup4.src.LoginActivity;
+import com.agilegroup4.src.MainMenuActivity;
 import com.agilegroup4.src.QuestionOverviewActivity;
 import com.agilegroup4.src.R;
 
@@ -32,11 +33,11 @@ public class QuestionIdTest extends ActivityInstrumentationTestCase2<LoginActivi
 	public void testLoginActivity() {
 
 		// Start monitor on intended activity.
-		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(QuestionOverviewActivity.class.getName(), null, false);
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(MainMenuActivity.class.getName(), null, false);
 
 		// Open login activity
 		LoginActivity loginActivity = getActivity();
-
+		
 		// Get the navigation option for user profile activity
 		final Button button = (Button) loginActivity.findViewById(com.agilegroup4.src.R.id.button_login);
 		final EditText inputText = (EditText) loginActivity.findViewById(com.agilegroup4.src.R.id.text_login_username);
@@ -52,17 +53,38 @@ public class QuestionIdTest extends ActivityInstrumentationTestCase2<LoginActivi
 			}
 		});
 
-		QuestionOverviewActivity questionOverviewActivity;
+		MainMenuActivity mainMenuActivity;
 
 		// Next activity is opened and casted to intended activity type.
 		try{
-			questionOverviewActivity = (QuestionOverviewActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 50000);
+			mainMenuActivity = (MainMenuActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 50000);
 		}catch(java.lang.ClassCastException e){
-			questionOverviewActivity = null;
+			mainMenuActivity = null;
 		}
 		
 		// We assert that the cast and fetch has gone right.
-		assertNotNull("Failed to go to question overview activity from login activity.", questionOverviewActivity);
+		assertNotNull("Failed to go to main menu activity from login activity.", mainMenuActivity);
+		
+		// Start monitor on intended activity.
+				ActivityMonitor activityMonitorTwo = getInstrumentation().addMonitor(QuestionOverviewActivity.class.getName(), null, false);
+
+				// Get the navigation option for question overview activity
+				final Button buttontwo = (Button) mainMenuActivity.findViewById(com.agilegroup4.src.R.id.button_questions);
+				mainMenuActivity.runOnUiThread(new Runnable() {
+				  @Override
+				  public void run() {
+					// Click button and open next activity.
+				    button.performClick();
+				  }
+				});
+
+				// Next activity is opened and casted to intended activity type.
+				QuestionOverviewActivity questionOverviewActivity = (QuestionOverviewActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 50000);
+				
+				// We assert that the cast and fetch has gone right.
+				assertNotNull("Failed to go to question overview from main menu activity", questionOverviewActivity);
+		
+		
 		
 		Question q = getFirstQuestWithComments(DatabaseHandler.getQuestions());
 		
@@ -73,13 +95,17 @@ public class QuestionIdTest extends ActivityInstrumentationTestCase2<LoginActivi
 		
 		int posInListView = questionOverviewActivity.idsTestMap.get(qid);
 		final ListView listview = (ListView) questionOverviewActivity.findViewById(R.id.listview);
-		//listView.performItemClick(
-			    //listView.getAdapter().getView(position, null, null), position, position);
+		listview.requestFocusFromTouch();
+		listview.setSelection(posInListView);
+		listview.performItemClick(
+			    listview.getAdapter().getView(posInListView, null, null), posInListView, posInListView);
 		
 		
 		//assertTrue(questionOverviewActivity.findViewById(com.agilegroup4.src.R.id.pop_text).toString().contains("Chris Jester-Young"));
 		//questionOverviewActivity.finish();
 		//loginActivity.finish();
+		
+		assertTrue(true);
 	}
 	
 	private Question getFirstQuestWithComments(ArrayList<Question> qs){

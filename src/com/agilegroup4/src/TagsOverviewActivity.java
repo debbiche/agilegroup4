ee2759106710339069519c5f1757078e282ac0df
JGrunden <it4grjo@student.chalmers.se>
2013-04-30 13:42:27 +0200

    Add tag functionality

--- src/com/agilegroup4/src/TagsOverviewActivity.java
+++ src/com/agilegroup4/src/TagsOverviewActivity.java
@@ -1,17 +1,58 @@
 package com.agilegroup4.src;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.agilegroup4.model.Tag;
+
 public class TagsOverviewActivity extends Activity {
+	
+	// Containing all the tags
+	private ArrayList<Tag> tags;
+	
+	// The currently displayed tags
+	private Tag topLeft;
+	private Tag topRight;
+	private Tag left;
+	private Tag center;
+	private Tag right;
+	private Tag bottomLeft;
+	private Tag bottomRight;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tags_overview);
+		
+		tags = createTestTags();
+		
+		if (tags.size() > 0) {
+			center = tags.get(0);
+			if (tags.size() > 1) {
+				right = tags.get(1);
+			}
+		}
+		
+		addRelatedTags();
+	}
+	
+	private void addRelatedTags(){
+		
+	}
+	
+	/**
+	 * Used for test purpose only
+	 */
+	private ArrayList<Tag> createTestTags(){
+		ArrayList<Tag> ret = new ArrayList<Tag>();
+		
+		return ret;
 	}
 
 	@Override

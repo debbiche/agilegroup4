c5543e05b45728c5e16f81069b7005c6642d296f
schroeja <schroeja@gmail.com>
2013-05-03 10:27:27 +0200

    removed generate tagDB function for now

--- src/com/agilegroup4/src/DatabaseHandler.java
+++ src/com/agilegroup4/src/DatabaseHandler.java
@@ -129,7 +129,7 @@ public class DatabaseHandler {
 	 * list view elements.
 	 */
 	public static void queryQuestions(int numberOfQuestions) {
-		DatabaseHandlerTagDB.createTagsDB();
+		//DatabaseHandlerTagDB.createTagsDB();
 
 		if (queriedQuestions == 0) {
 			Cursor questionsCursor = db

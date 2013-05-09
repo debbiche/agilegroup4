d12b2c65a3ad67e3ebc5f0b462c155676deb3092
Markus Pettersson <pmarkus@student.chalmers.se>
2013-05-09 10:28:05 +0200

    Fixed the free text search

--- src/com/agilegroup4/src/MainMenuActivity.java
+++ src/com/agilegroup4/src/MainMenuActivity.java
@@ -1,5 +1,7 @@
 package com.agilegroup4.src;
 
+import com.agilegroup4.model.SearchMode;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -44,6 +46,7 @@ public class MainMenuActivity extends Activity {
 	@Override 
 	public boolean onSearchRequested() {
 	    System.out.println("search pressed");
+	    SearchableActivity.SearchMode = SearchMode.QUESTION_FREETEXT;
 	    return super.onSearchRequested();
 	}
 

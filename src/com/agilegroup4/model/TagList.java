16dbd2d83ec60c85beb79e1b66246606bf10ff50
unknown <mikajoha85@gmail.com>
2013-05-16 08:24:24 +0200

    Dummy commit

--- src/com/agilegroup4/model/TagList.java
+++ src/com/agilegroup4/model/TagList.java
@@ -66,7 +66,11 @@ public class TagList extends ArrayList<Tag> {
 		ArrayList<Tag> tags = new ArrayList<Tag>();
 		ArrayList<Tag> exceptTags = new ArrayList<Tag>(this);
 		for (Tag tag : this)
-			tags.addAll(tag.getRelatedTags(MAX_NR_OF_COMBINATIONS/this.size(), exceptTags.addAll(tags)));
+		{	
+			tags.addAll(tag.getRelatedTags(MAX_NR_OF_COMBINATIONS/this.size(), exceptTags));
+			//TODO: Add all withpout duplicates!!
+			exceptTags.addAll(tags);
+		}
 		return tags;
 	}
 }
